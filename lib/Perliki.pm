700b91043874e7bbaa843951e9e92eba63e9fd63
vti <viacheslav.t@gmail.com>
2012-07-14 11:49:20 +0300

    Updated contrib files

--- lib/Perliki.pm
+++ lib/Perliki.pm
@@ -10,7 +10,6 @@ use Turnaround::ActionFactory;
 use Turnaround::Dispatcher::Routes;
 use Turnaround::Displayer;
 use Turnaround::HelperFactory;
-use Turnaround::I18N;
 use Turnaround::Renderer::Caml;
 use Turnaround::Routes::FromConfig;
 
@@ -29,8 +28,6 @@ sub startup {
     $self->{config} = $config;
 
     Perliki::DB->init_db(%{$config->{database}});
-
-    my $i18n = Turnaround::I18N->new(app_class => __PACKAGE__);
 
     my $displayer = Turnaround::Displayer->new(
         renderer => Turnaround::Renderer::Caml->new(home => $self->{home}),
@@ -57,8 +54,6 @@ sub startup {
         secret  => $config->{session}->{secret},
         expires => $config->{session}->{expires}
     );
-
-    $self->add_middleware('I18N', i18n => $i18n);
 
     $self->add_middleware(
         'RequestDispatcher',
@@ -105,21 +100,7 @@ sub startup {
             sub {
                 my $env = shift;
 
-                $env->{'turnaround.displayer.vars'}->{'helpers'} =
-                  Turnaround::HelperFactory->new(
-                    namespace => 'Perliki::Helper::');
-
                 $env->{'turnaround.displayer.vars'}->{'title'} = $config->{wiki}->{title};
-
-                my $languages_names = $i18n->get_languages_names;
-                if (keys %$languages_names > 1) {
-                    $env->{'turnaround.displayer.vars'}->{'languages'} =
-                      [map { {code => $_, name => $languages_names->{$_}} }
-                          keys %$languages_names];
-                }
-
-                $env->{'turnaround.displayer.vars'}->{'loc'} =
-                  sub { shift; $env->{'turnaround.i18n.maketext'}->loc(@_) };
 
                 return $app->($env);
               }
@@ -127,6 +108,8 @@ sub startup {
     );
 
     $self->add_middleware('ViewDisplayer', displayer => $displayer);
+
+    $self->register_plugin('I18N');
 
     return $self;
 }
